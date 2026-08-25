6dde358decc3e27e4d7cb50a2f74e66bb4bf6db3
juane <juanestebancorrea2004@gmail.com>
2026-08-25 14:47:17 -0500

    fix: quitar anotacion NonNull obsoleta en JwtAuthenticationFilter

--- src/main/java/co/edu/upb/veterinaria/security/JwtAuthenticationFilter.java
+++ src/main/java/co/edu/upb/veterinaria/security/JwtAuthenticationFilter.java
@@ -7,7 +7,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -38,9 +37,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	}
 
 	@Override
-	protected void doFilterInternal(@NonNull HttpServletRequest request,
-									@NonNull HttpServletResponse response,
-									@NonNull FilterChain filterChain) throws ServletException, IOException {
+	protected void doFilterInternal(HttpServletRequest request,
+									HttpServletResponse response,
+									FilterChain filterChain) throws ServletException, IOException {
 
 		String cabecera = request.getHeader("Authorization");
 
